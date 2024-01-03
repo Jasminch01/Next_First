3b5951c0602a993490819e5c2e2613943e17c5bd
jasmin <jasminchakma895@gmail.com>
2024-01-03 22:00:02 +0600

    link component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Counter from "@/Components/Counter/Counter";
 import React from "react";
-import error from "./error";
+// import error from "./error";
+import Link from "next/link";
 
 const page = () => {
   // throw new error;
@@ -8,6 +9,17 @@ const page = () => {
     <div>
       <p className="text-4xl font-bold">hello world</p>
       <Counter />
+      <div className="mt-5 space-x-3">
+        <Link href={"/about"}>
+          <bttton className="p-2 bg-orange-200">About</bttton>
+        </Link>
+        <Link href={"/news"}>
+          <bttton className="p-2 bg-orange-200">News</bttton>
+        </Link>
+        <Link href={"/contact"}>
+          <bttton className="p-2 bg-orange-200"> Contact</bttton>
+        </Link>
+      </div>
     </div>
   );
 };
