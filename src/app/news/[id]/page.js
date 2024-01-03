80468943dd852ad67901ba20ea2e9f4db809c32e
jasmin <jasminchakma895@gmail.com>
2024-01-03 20:15:11 +0600

    search params

--- src/app/news/[id]/page.js
+++ src/app/news/[id]/page.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const DynamicPage = ({params}) => {
+const DynamicPage = ({params, searchParams}) => {
     console.log(params)
+    console.log(searchParams)
     return (
         <div>
             <p>This is DynamicPage : {params.id}</p>
