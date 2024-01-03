077d1909828c351b565fa96fdbf5887d37130b1f
jasmin <jasminchakma895@gmail.com>
2024-01-03 22:24:33 +0600

    import ilias

--- src/app/album/page.js
+++ src/app/album/page.js
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
+import image from '@/assets/pexels-pixabay-158361.jpg'
 
 const AlbumPage = () => {
     return (
         <div>
             <Image src={`https://i.ibb.co/F4f5J0d/pexels-julia-m-cameron-4144294.jpg`} alt='album image' height={300} width={500}/>
+
+            <Image src={image} alt='album image' height={300} width={500}/>
         </div>
     );
 };
